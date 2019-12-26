21ffda27fbf852956244aa4768a663c4d5a00bf8
zjun <565061763@qq.com>
2019-12-26 10:11:14 +0800

    demo

--- app/src/main/java/com/sd/myandroid/statusbar/MainActivity.java
+++ app/src/main/java/com/sd/myandroid/statusbar/MainActivity.java
@@ -1,16 +1,19 @@
 package com.sd.myandroid.statusbar;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.sd.lib.statusbar.FStatusBarUtils;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FStatusBarUtils.setTransparent(getWindow(), false);
     }
 }
