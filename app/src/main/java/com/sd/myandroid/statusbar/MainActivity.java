07f60293b726aa1b75cc4457424448b31d6111dd
zjun <565061763@qq.com>
2019-12-26 16:08:45 +0800

    demo

--- app/src/main/java/com/sd/myandroid/statusbar/MainActivity.java
+++ app/src/main/java/com/sd/myandroid/statusbar/MainActivity.java
@@ -1,19 +1,31 @@
 package com.sd.myandroid.statusbar;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.sd.lib.statusbar.FStatusBarUtils;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private boolean mDarkStatusBar = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FStatusBarUtils.setTransparent(getWindow(), false);
+        FStatusBarUtils.setTransparent(getWindow(), mDarkStatusBar);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        mDarkStatusBar = !mDarkStatusBar;
+        FStatusBarUtils.setTransparent(getWindow(), mDarkStatusBar);
     }
 }
