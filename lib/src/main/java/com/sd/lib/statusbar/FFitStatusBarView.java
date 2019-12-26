7b91a2b99518ac715c08ee3b2835bda148865ee9
zjun <565061763@qq.com>
2019-12-26 09:57:32 +0800

    first commit

---
+++ lib/src/main/java/com/sd/lib/statusbar/FFitStatusBarView.java
@@ -0,0 +1,56 @@
+package com.sd.lib.statusbar;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+public class FFitStatusBarView extends FrameLayout
+{
+    private boolean mFitStatusBar = true;
+
+    public FFitStatusBarView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+
+    public void setFitStatusBar(boolean fit)
+    {
+        if (mFitStatusBar != fit)
+        {
+            mFitStatusBar = fit;
+            checkFitStatusBar();
+        }
+    }
+
+    public void checkFitStatusBar()
+    {
+        if (mFitStatusBar)
+        {
+            final int statusBarHeight = FStatusBarUtils.getActivityStatusBarHeight(getContext());
+            super.setPadding(getPaddingLeft(), statusBarHeight, getPaddingRight(), getPaddingBottom());
+        } else
+        {
+            super.setPadding(getPaddingLeft(), 0, getPaddingRight(), getPaddingBottom());
+        }
+    }
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom)
+    {
+    }
+
+    @Override
+    protected void onConfigurationChanged(Configuration newConfig)
+    {
+        super.onConfigurationChanged(newConfig);
+        checkFitStatusBar();
+    }
+
+    @Override
+    protected void onAttachedToWindow()
+    {
+        super.onAttachedToWindow();
+        checkFitStatusBar();
+    }
+}
---
+++ lib/src/main/java/com/sd/lib/statusbar/FStatusBarUtils.java
@@ -0,0 +1,88 @@
+package com.sd.lib.statusbar;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class FStatusBarUtils
+{
+    /**
+     * 全透明状态栏
+     */
+    public static void setTransparent(Window window)
+    {
+        if (Build.VERSION.SDK_INT >= 21)
+        {
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= 19)
+        {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 半透明状态栏
+     */
+    public static void setHalfTransparent(Window window)
+    {
+        if (Build.VERSION.SDK_INT >= 21)
+        {
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        } else if (Build.VERSION.SDK_INT >= 19)
+        {
+            window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //虚拟键盘也透明
+            // getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+        }
+    }
+
+    /**
+     * 状态栏高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusBarHeight(Context context)
+    {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0)
+        {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    /**
+     * Activity的状态栏高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getActivityStatusBarHeight(Context context)
+    {
+        if (context instanceof Activity)
+        {
+            final Activity activity = (Activity) context;
+            final boolean isStatusBarVisible = ((activity.getWindow().getAttributes().flags & WindowManager.LayoutParams.FLAG_FULLSCREEN) == 0);
+
+            if (isStatusBarVisible)
+                return getStatusBarHeight(context);
+            else
+                return 0;
+        } else
+        {
+            return getStatusBarHeight(context);
+        }
+    }
+}
